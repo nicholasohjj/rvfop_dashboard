1b7426172324cb6429ba6170d9c2097c7fe9a2e4
Nicholas Oh <nicholasohjj@gmail.com>
2024-03-01 21:59:30 +0800

    env variables

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5Z2Z6Znl5a2lyc2huYW5icHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYzNTgyNjUsImV4cCI6MjAyMTkzNDI2NX0.sVaq8OpUPK1Fb0S9nMTgcgE_JRRL0_dB4fp6_8FmhJs";
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
     db: {
