7b1a4d3b718ac0501020ae730efc82e9e50404ff
Nicholas Oh <nicholasohjj@gmail.com>
2024-07-02 10:05:49 +0700

    img in about apge

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -32,7 +32,19 @@ const About = () => {
         </WindowHeader>
 
         <WindowContent style={{ padding: "20px", overflowY: "auto" }}>
-          <p>
+          <div style={{ display: "flex", justifyContent: "center", marginBottom: 8 }}>
+            <img
+              src="https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/logo.png"
+              alt="rvrc-logo"
+              width={100}
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                window.open("https://www.instagram.com/rvrcfop", "_blank");
+              }}
+            />
+          </div>
+
+          <p style={{ textAlign: "left" }}>
             Welcome to the Freshmen Orientation Programme (FOP) AY24/25 at RVRC!
             Embrace an unforgettable journey where freshmen from the 7 tribal houses unite under the theme "Insieme" (Italian for "together"). Join us as we celebrate RVRC's 10th anniversary with immersive activities, fostering camaraderie, teamwork, and environmental stewardship. Navigate challenges, forge alliances, and assemble the Quark-Working Enthalpy Energiser Nuke (QWEEN) to secure victory. Discover your role in this epic narrative and integrate into RVRC's vibrant community through engaging gameplay and vibrant, anime-inspired visuals. Welcome to a journey of unity, resilience, and lasting memories.
           </p>
