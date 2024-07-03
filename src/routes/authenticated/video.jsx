96aa831ea28836f5411f7c93474d7d218577da0b
Nicholas Oh <nicholasohjj@gmail.com>
2024-07-03 14:25:49 +0700

    edited videos

--- src/routes/authenticated/video.jsx
+++ src/routes/authenticated/video.jsx
@@ -8,6 +8,10 @@ const videos = [
   "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/Bongo_Cat_-_BLACKPINK__DDU-DU_DDU-DU___K-POP_.mp4?t=2024-06-11T15%3A21%3A42.696Z",
   "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/_bongo_cat_and_friends__meow.mp4?t=2024-06-11T15%3A23%3A25.461Z",
   "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/videoplayback.mp4?t=2024-06-11T15%3A29%3A05.571Z",
+  "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/Y2meta.app___Let_It_Go_(cover_by_Bongo_Cat)____(720p).mp4?t=2024-07-03T07%3A21%3A31.274Z",
+  "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/Y2meta.app_BTS_(_____)__Dynamite____(cover_by_Bongo_Cat)_____(480p).mp4?t=2024-07-03T07%3A23%3A50.359Z",
+  "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/Y2meta.app___Imagine_Cats___Believer____(720p).mp4?t=2024-07-03T07%3A24%3A44.082Z",
+  "https://tygfzfyykirshnanbprr.supabase.co/storage/v1/object/public/rvfop/Y2meta.app___Ed_Sheeran___Shape_of_You_(cover_by_Bongo_Cat)____(1080p).mp4?t=2024-07-03T07%3A25%3A38.843Z"
 ];
 
 const Video = () => {
