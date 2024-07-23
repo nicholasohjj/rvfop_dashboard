91c9a492ff5d1de811514e42fd6be91b54eb009a
Nicholas Oh <nicholasohjj@gmail.com>
2024-07-23 13:37:39 +0700

    modified link markup

--- src/utils/createLinkMarkup.jsx
+++ src/utils/createLinkMarkup.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import { Anchor } from "react95";
 
 export const createLinkMarkup = (text) => {
-  const urlPattern =
-    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])|(\bwww\.[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])|(\bwww\.[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
   const parts = text.split(urlPattern);
   return parts.map((part, index) => {
-    if (
-      /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])|(\bwww\.[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi.test(
-        part
-      )
-    ) {
-      const url = part.startsWith("www.") ? `http://${part}` : part;
+    if (urlPattern.test(part)) {
+      const url = part.toLowerCase().startsWith("www.") ? `http://${part}` : part;
       return (
-        <Anchor key={index} href={url} target="_blank">
+        <Anchor key={index} href={url} target="_blank" rel="noopener noreferrer">
           {part}
         </Anchor>
       );
